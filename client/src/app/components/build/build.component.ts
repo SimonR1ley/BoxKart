e1ff79b98b08e63a3b30338dce03edbbe31b1950
SimonBRiley <43470764+SimonR1ley@users.noreply.github.com>
2023-04-11 08:59:21 +0200

    Added Models

--- client/src/app/components/build/build.component.ts
+++ client/src/app/components/build/build.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit, AfterViewInit, Input, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  Input,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import * as THREE from "three";
+import * as THREE from 'three';
 import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
 
@@ -8,35 +15,26 @@ import { Items } from 'src/app/models/Items';
 import { ItemdbService } from 'src/app/services/itemdb.service';
 import { Router } from '@angular/router';
 
-
-
-
 @Component({
   selector: 'app-build',
   templateUrl: './build.component.html',
   styleUrls: ['./build.component.css'],
-  providers: [ItemdbService]
+  providers: [ItemdbService],
 })
 export class BuildComponent implements OnInit, AfterViewInit {
+  boxBuild: string = '../../../assets/models/BoxKart.gltf';
 
-
-  boxBuild: string = "../../../assets/models/BoxKart.gltf"
-
-  buildUrl: String = ""
+  buildUrl: String = '';
 
   public build = [
-  {
-    body: "",
-    wheels: "",
-    arial: ""
-  }
-  ]
+    {
+      body: '',
+      wheels: '',
+      arial: '',
+    },
+  ];
 
-
-
- 
-
-  constructor(private itemService: ItemdbService, private router:Router){}
+  constructor(private itemService: ItemdbService, private router: Router) {}
 
   filter: String = 'Body';
 
@@ -51,15 +49,12 @@ export class BuildComponent implements OnInit, AfterViewInit {
   ngOnInit() {
     this.itemService.getAllItems().subscribe((data) => {
       console.log(data);
-      
+
       this.allItems = data;
-    })
-  //  this.allItems = this.itemService.getAllItems()
+    });
+    //  this.allItems = this.itemService.getAllItems()
   }
 
-
-
- 
   get Items() {
     // if (this.filter === 'All') {
     //   return this.allItems;
@@ -80,40 +75,12 @@ export class BuildComponent implements OnInit, AfterViewInit {
       return this.allItems.filter((item) =>
         this.filter === 'Arial' ? item.type === 'Arial' : !item.type
       );
-    }
-    else{
-      return null
+    } else {
+      return null;
     }
   }
 
-
-
-  craftKart(){
-    
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  craftKart() {}
 
   @ViewChild('canvas') private canvasRef: ElementRef;
 
@@ -167,14 +134,13 @@ export class BuildComponent implements OnInit, AfterViewInit {
       // this.model.rotation.y += 0.005;
     }
   }
-   
-  private createControls = () => {
 
+  private createControls = () => {
     let _width = 300;
-		let _height = 300;
+    let _height = 300;
 
     const renderer = new CSS2DRenderer();
-    renderer.setSize(_width, _height)
+    renderer.setSize(_width, _height);
 
     // renderer.domElement.style.position = 'absolute';
     // renderer.domElement.style.zIndex = '1';
@@ -183,7 +149,10 @@ export class BuildComponent implements OnInit, AfterViewInit {
     renderer.domElement.style.borderRadius = '20px';
     renderer.domElement.style.width = '100%';
     renderer.domElement.style.height = '100%';
-    document.getElementById('model-con')?.appendChild(renderer.domElement).setAttribute("id", "controls");
+    document
+      .getElementById('model-con')
+      ?.appendChild(renderer.domElement)
+      .setAttribute('id', 'controls');
     this.controls = new OrbitControls(this.camera, renderer.domElement);
     this.controls.autoRotate = true;
     this.controls.enableZoom = false;
@@ -203,10 +172,10 @@ export class BuildComponent implements OnInit, AfterViewInit {
       aspectRatio,
       this.nearClippingPane,
       this.farClippingPane
-    )
-    this.camera.position.x = 700;
-    this.camera.position.y = 300;
-    this.camera.position.z = 500;
+    );
+    this.camera.position.x = 800;
+    this.camera.position.y = 10;
+    this.camera.position.z = 350;
     this.ambientLight = new THREE.AmbientLight(0x00000, 20);
     this.scene.add(this.ambientLight);
     this.directionalLight = new THREE.DirectionalLight(0xffffff, 0);
@@ -234,7 +203,11 @@ export class BuildComponent implements OnInit, AfterViewInit {
   private startRenderingLoop() {
     //* Renderer
     // Use canvas element in template
-    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas, antialias: true, alpha: true });
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      antialias: true,
+      alpha: true,
+    });
     // this.renderer.setClearColor( 0x000000, 0.3);
     this.renderer.setPixelRatio(devicePixelRatio);
     this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
@@ -243,9 +216,8 @@ export class BuildComponent implements OnInit, AfterViewInit {
       component.renderer.render(component.scene, component.camera);
       component.animateModel();
       requestAnimationFrame(render);
-    }());
+    })();
   }
-
 
   ngAfterViewInit() {
     this.createScene();
@@ -253,53 +225,353 @@ export class BuildComponent implements OnInit, AfterViewInit {
     this.createControls();
   }
 
-
-
-  public buildKart(item: any, index: any){
-    
-    if(item.type === "Body"){
-      this.build[0] = {...this.build[0],body: item.name}
+  public buildKart(item: any, index: any) {
+    if (item.type === 'Body') {
+      this.build[0] = { ...this.build[0], body: item.name };
     }
 
-    if(item.type === "Wheel"){
-      this.build[0] = {...this.build[0],wheels: item.name}
+    if (item.type === 'Wheel') {
+      this.build[0] = { ...this.build[0], wheels: item.name };
     }
 
-    if(item.type === "Arial"){
-      this.build[0] = {...this.build[0],arial: item.name}
+    if (item.type === 'Arial') {
+      this.build[0] = { ...this.build[0], arial: item.name };
     }
-    
+
     console.log(this.build);
 
-
-    // -----------Conditions---------- 
+    // -----------Conditions----------
 
     this.scene.remove(this.model);
 
-
     // ---------Bodies---------
- 
-    if(this.build[0].body === "Box" && this.build[0].wheels === "" && this.build[0].arial === ""){
-      this.boxBuild = "../../../assets/models/Box.gltf";
-     this.reloadScene(this.boxBuild) 
+
+    // Box
+
+    if (
+      this.build[0].body === 'Box' &&
+      this.build[0].wheels === '' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/Box.gltf';
+      this.reloadScene(this.boxBuild);
     }
 
+    // Fridge
 
-      // ---------Bodies & Wheels---------
-
-    if(this.build[0].body === "Box" && this.build[0].wheels === "Doughnut" && this.build[0].arial === ""){
-      this.boxBuild = "../../../assets/models/BoxDoughnutWheels.gltf";
-     this.reloadScene(this.boxBuild)
+    if (
+      this.build[0].body === 'Fridge' &&
+      this.build[0].wheels === '' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/Fridge.gltf';
+      this.reloadScene(this.boxBuild);
     }
 
+    // Coffin
 
-     // ---------Bodies & Wheels & Arials---------
+    if (
+      this.build[0].body === 'Coffin' &&
+      this.build[0].wheels === '' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/Coffin.gltf';
+      this.reloadScene(this.boxBuild);
+    }
 
+    // Bathtub
 
+    if (
+      this.build[0].body === 'Bathtub' &&
+      this.build[0].wheels === '' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/Bathtub.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // ---------Bodies & Wheels---------
+
+    // Body + Doughnut
+
+    // Box + Doughnut
+
+    if (
+      this.build[0].body === 'Box' &&
+      this.build[0].wheels === 'Doughnut' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/BoxDoughnutWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Coffin + Doughnut
+
+    if (
+      this.build[0].body === 'Coffin' &&
+      this.build[0].wheels === 'Doughnut' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/CoffinDoughnutWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Fridge + Doughnut
+
+    if (
+      this.build[0].body === 'Fridge' &&
+      this.build[0].wheels === 'Doughnut' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/FridgeDoughnutWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Bathtub + Doughnut
+
+    if (
+      this.build[0].body === 'Bathtub' &&
+      this.build[0].wheels === 'Doughnut' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/BathtubDoughnutWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Body + Saw
+
+    // Box + Saw
+
+    if (
+      this.build[0].body === 'Box' &&
+      this.build[0].wheels === 'Saw' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/BoxSawWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Coffin + Saw
+
+    if (
+      this.build[0].body === 'Coffin' &&
+      this.build[0].wheels === 'Saw' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/CoffinSawWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Fridge + Saw
+
+    if (
+      this.build[0].body === 'Fridge' &&
+      this.build[0].wheels === 'Saw' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/FridgeSawWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Bathtub + Saw
+
+    if (
+      this.build[0].body === 'Bathtub' &&
+      this.build[0].wheels === 'Saw' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/BathtubSawWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Body + Wagon
+
+    // Box + Wagon
+
+    if (
+      this.build[0].body === 'Box' &&
+      this.build[0].wheels === 'Wagon' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/BoxWagonWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Coffin + Wagon
+
+    if (
+      this.build[0].body === 'Coffin' &&
+      this.build[0].wheels === 'Wagon' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/CoffinWagonWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Fridge + Wagon
+
+    if (
+      this.build[0].body === 'Fridge' &&
+      this.build[0].wheels === 'Wagon' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/FridgeWagonWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Bathtub + Wagon
+
+    if (
+      this.build[0].body === 'Bathtub' &&
+      this.build[0].wheels === 'Wagon' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/BathtubWagonWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Body + Cheese
+
+    // Box + Cheese
+
+    if (
+      this.build[0].body === 'Box' &&
+      this.build[0].wheels === 'Cheese' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/BoxCheeseWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Coffin + Cheese
+
+    if (
+      this.build[0].body === 'Coffin' &&
+      this.build[0].wheels === 'Cheese' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/CoffinCheeseWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Fridge + Cheese
+
+    if (
+      this.build[0].body === 'Fridge' &&
+      this.build[0].wheels === 'Cheese' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/FridgeCheeseWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Bathtub + Cheese
+
+    if (
+      this.build[0].body === 'Bathtub' &&
+      this.build[0].wheels === 'Cheese' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/BathtubCheeseWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Body + Record
+
+    // Box + Record
+
+    if (
+      this.build[0].body === 'Box' &&
+      this.build[0].wheels === 'Record' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/BoxRecordWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Coffin + Record
+
+    if (
+      this.build[0].body === 'Coffin' &&
+      this.build[0].wheels === 'Record' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/CoffinRecordWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Fridge + Record
+
+    if (
+      this.build[0].body === 'Fridge' &&
+      this.build[0].wheels === 'Record' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/FridgeRecordWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Bathtub + Record
+
+    if (
+      this.build[0].body === 'Bathtub' &&
+      this.build[0].wheels === 'Record' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/BathtubRecordWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Body + Stone
+
+    // Box + Stone
+
+    if (
+      this.build[0].body === 'Box' &&
+      this.build[0].wheels === 'Stone' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/BoxStoneWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Coffin + Stone
+
+    if (
+      this.build[0].body === 'Coffin' &&
+      this.build[0].wheels === 'Stone' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/CoffinStoneWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Fridge + Stone
+
+    if (
+      this.build[0].body === 'Fridge' &&
+      this.build[0].wheels === 'Stone' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/FridgeStoneWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // Bathtub + Stone
+
+    if (
+      this.build[0].body === 'Bathtub' &&
+      this.build[0].wheels === 'Stone' &&
+      this.build[0].arial === ''
+    ) {
+      this.boxBuild = '../../../assets/models/BathtubStoneWheels.gltf';
+      this.reloadScene(this.boxBuild);
+    }
+
+    // ---------Bodies & Wheels & Arials---------
   }
 
-
-  reloadScene(boxBuild: string){
+  reloadScene(boxBuild: string) {
     this.loaderGLTF.load(boxBuild, (gltf: GLTF) => {
       this.model = gltf.scene.children[0];
       console.log(this.model);
@@ -309,5 +581,4 @@ export class BuildComponent implements OnInit, AfterViewInit {
       this.scene.add(this.model);
     });
   }
-
 }
